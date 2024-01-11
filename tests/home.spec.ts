219b2c93c0beae20cb24bf71aba12c13fd313ccc
Bruno de Avila Lima <br.avila.l@gmail.com>
2024-01-11 17:42:13 -0300

    Teste

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-test('Webapp deve estar online', async ({ page })=>{
+test('Webapp deve estar online e acessível', async ({ page })=>{
     await page.goto('http://localhost:3000/')
     await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
 })
