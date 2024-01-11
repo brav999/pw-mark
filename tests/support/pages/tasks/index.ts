55d90cb9567a17826179258c2ed153c4e3c824d3
Bruno de Avila Lima <br.avila.l@gmail.com>
2024-01-11 17:39:13 -0300

    first commit

---
+++ tests/support/pages/tasks/index.ts
@@ -0,0 +1,35 @@
+import { Page, expect } from '@playwright/test'
+import { TaskModel } from '../../../fixtures/task.model'
+
+
+export class TasksPage{
+    readonly page:Page
+
+    constructor(page: Page){
+        this.page = page
+    }
+
+    async go(){
+        await this.page.goto('http://localhost:3000/')
+    }
+    
+    async create(task: TaskModel){
+        const inputTaskName = this.page.locator('input[class*=InputNewTask]')
+        await inputTaskName.fill(task.name)
+        
+        await this.page.click('css=button >> text=Create')
+    }
+
+    async shouldHaveText(taskName: string){
+        const target = this.page.getByTestId(`css=.task-item p >> test=${taskName}`)
+        await expect(target).toBeVisible
+    }
+
+    async  alertHaveText(text: string){
+        const target = this.page.locator('.swal2-html-container')
+        await expect(target).toHaveText(text)
+    }
+
+
+
+}
---
+++ tests/tasks.spec.ts
@@ -0,0 +1,34 @@
+import { test } from "@playwright/test";
+import { TaskModel } from "./fixtures/task.model";
+import { deleteTaskByHelper, postTask } from "./support/helpers";
+import { TasksPage } from "./support/pages/tasks";
+
+test('deve permitir cadastrar uma nova tarefa', async ({page, request}) => {
+    const task: TaskModel = {
+        name: 'Ler um livro de TypeScript',
+        is_done: false
+    }
+    await deleteTaskByHelper(request, task.name)
+
+    const tasksPage: TasksPage = new TasksPage(page)
+
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.shouldHaveText(task.name)
+})
+
+test('não deve permitir tarefa duplicada', async ({page, request}) => {
+    const task: TaskModel = {
+        name: 'Comprar Ketchup',
+        is_done: false
+    }
+
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
+
+    const tasksPage: TasksPage = new TasksPage(page)
+    
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.alertHaveText('Task already exists!')
+})
